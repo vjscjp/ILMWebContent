9ef0c5a6b54d128f8f36ea0017e6fad528e384fa
sandeepnu <sandeeptailor2000@gmail.com>
2015-09-23 00:19:01 +0530

    ExtraFiles

---
+++ frmsadmin/extraFiles/recordsNav.js
@@ -0,0 +1,47 @@
+/*global define:true*/
+define(["dojo/dom", "dojo/on", "dojo/_base/lang"], function(dom, on, lang) {
+    "use strict";
+    return {
+        init: function() {
+            /*var create = dom.byId("btnCreateRecord_recordsNav"),
+            	modify = dom.byId("btnModifyRecord_recordsNav"),
+                search = dom.byId("btnSearchRecords_recordsNav");*/
+            on(this.btnCreateRecords_recordsNav, "click", lang.hitch(this, function(e) {
+                //this.transitionTo(e, "record");
+                var transOpts = {
+                        target: targetView,
+                        data : {
+                        	displaySearch:false,
+							type:'',
+							path:'',
+							hasUserFormRWAccess:''
+						}
+                    };
+                    this.app.transitionToView(e.target, transOpts);
+            }));
+            on(this.btnModifyRecords_recordsNav, "click", lang.hitch(this, function(e) {
+                //this.transitionTo(e, "records");
+                var transOpts = {
+                        target: targetView,
+                        data : {
+                        	displaySearch:true,
+							type:'',
+							path:'',
+							hasUserFormRWAccess:''
+						}
+                    };
+                    this.app.transitionToView(e.target, transOpts);
+            }));
+            /*on(search, "click", lang.hitch(this, function(e) {
+                this.transitionTo(e, "recordsSearch");
+            }));*/
+        },
+        transitionTo: function(e, targetView) {
+            var transOpts = {
+                target: targetView,
+                params: null
+            };
+            this.app.transitionToView(e.target, transOpts);
+        }
+    };
+});
